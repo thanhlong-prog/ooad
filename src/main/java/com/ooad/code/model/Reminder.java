5346f1b945dc5d30fc72317d1e35c42fa9d27fd1
Pham Thanh <thanhlongtivip@gmail.com>
2025-05-17 22:14:46 +0700

    fix thym

---
+++ src/main/java/com/ooad/code/model/Reminder.java
@@ -0,0 +1,32 @@
+package com.ooad.code.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "reminders") 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Reminder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "type")
+    private String type; 
+
+    @ManyToOne
+    private Appointment appointment;
+}
